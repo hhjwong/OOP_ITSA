d4acd407933c45b321d5293f851cd463702ab79b
hhjwong <79494918+hhjwong@users.noreply.github.com>
2022-06-08 18:17:53 +0800

    Create MM117.cpp

---
+++ MM117.cpp
@@ -0,0 +1,41 @@
+#include<iostream>  
+#include<cstdlib>  
+#include<cstring>      
+  
+using namespace std;   
+      
+int main(){    
+    char input[2000];    
+    int round;    
+    cin>>round;  
+    for(int t=0;t<round;t++){    
+        cin>>input;    
+        char partition[4];    
+        int result=0,index=strlen(input),reps=0;    
+        index-=3;    
+        while(index>=0){    
+            strncpy(partition,input+index,3);    
+            index-=3;    
+            if((reps/2)*2==reps)    
+                result=result+atoi(partition);    
+            else    
+                result=result-atoi(partition);    
+            reps++;    
+        }    
+        int remain=3+index;    
+        if(index!=0){    
+            memset(partition,'\0',sizeof(partition));    
+            strncpy(partition,input,remain);    
+            if((reps/2)*2==reps)    
+                result=result+atoi(partition);    
+            else    
+                result=result-atoi(partition);    
+            reps++;    
+        }    
+        if(result%7==0)    
+            cout<<"true\n";    
+        else      
+            cout<<"false\n";    
+    }    
+    return 0;      
+}  
